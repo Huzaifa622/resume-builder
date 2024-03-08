0632f5eeee582012f6c47e1e4e9fe85c8526dbcc
huzaifa622 <thuzaifa07@gmail.com>
2024-03-08 18:42:03 +0500

    create resume builder

---
+++ app/components/Resume/ResumeControlBar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import {
+  ArrowDownTrayIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import dynamic from "next/dynamic";
+import { useSetDefaultScale } from "./hooks";
+import { usePDF } from "@react-pdf/renderer";
+import { useEffect, useState } from "react";
+
+const ResumeControlBar = ({
+  scale,
+  setScale,
+  documentSize,
+  document,
+  fileName,
+}: {
+  scale: number;
+  setScale: (scale: number) => void;
+  documentSize: string;
+  document: JSX.Element;
+  fileName: string;
+}) => {
+  const { scaleOnResize, setScaleOnResize } = useSetDefaultScale({
+    setScale,
+    documentSize,
+  });
+  const [data, setData] = useState({});
+  const [instance, update] = usePDF({ document });
+
+  useEffect(() => {
+    update(document);
+  }, [update, document]);
+  useEffect(() => {
+    setData(
+      JSON.parse(window.localStorage.getItem("resume-builder-parser-state")!)
+    );
+  }, []);
+  return (
+    <div className="sticky bottom-0 left-0 right-0 flex h-[var(--resume-control-bar-height)]  items-center justify-center px-[var(--resume-padding)] text-gray-600 lg:justify-between">
+      <a
+        className="ml-1 flex items-center gap-1 rounded-md border border-gray-300 px-3 py-0.5 hover:bg-gray-100 lg:ml-8 cursor-pointer"
+        // href={instance.url!}
+        // download={fileName}
+        onClick={(e) => {
+          if (!data) {
+            window.alert("No required field");
+          }
+          // @ts-ignore
+          const resume = data.resume;
+          // @ts-ignore
+          const educations = resume.educations;
+          // @ts-ignore
+          const profile = resume.profile;
+          // console.log(data);
+          // console.log(educations, profile);
+          e.currentTarget.href = instance.url!;
+          e.currentTarget.download = fileName;
+        }}
+      >
+        <ArrowDownTrayIcon className="h-4 w-4" />
+        <span className="whitespace-nowrap">Download Resume</span>
+      </a>
+    </div>
+  );
+};
+
+export const ResumeControlBarCSR = dynamic(
+  () => Promise.resolve(ResumeControlBar),
+  {
+    ssr: false,
+  }
+);
---
+++ app/home/Hero.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { FlexboxSpacer } from "../components/FlexboxSpacer";
+import { AutoTypingResume } from "./AutoTypingResume";
+
+export const Hero = () => {
+  return (
+    <section className="lg:flex lg:h-[825px] lg:justify-center">
+      <FlexboxSpacer maxWidth={75} minWidth={0} className="hidden lg:block" />
+      <div className="mx-auto max-w-xl pt-8 text-center lg:mx-0 lg:grow lg:pt-32 lg:text-left">
+        <h1 className=" pb-2 text-4xl font-bold lg:text-5xl">
+          Create a professional
+          <br />
+          resume easily
+        </h1>
+        <p className="mt-3 mb-14 text-lg lg:mt-5 lg:text-xl">
+          With this powerful resume builder
+        </p>
+        <Link
+          href="/resume-import"
+          className="bg-black px-4 py-2 rounded-full mt-8 text-white lg:mt-14"
+        >
+          Create Resume
+        </Link>
+      </div>
+    </section>
+  );
+};
